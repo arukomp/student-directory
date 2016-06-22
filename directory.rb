3407dfdcc86741133e37fc1291243cec90a443d4
Arunas Skirius <arukomp@gmail.com>
2016-06-22 11:14:57 +0100

    Added a few comments and the total number of students

---
+++ directory.rb
@@ -0,0 +1,18 @@
+# first we print the list of students
+puts "The students of Villains Academy"
+puts "------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+# finally, we print the total number of students
+print "Oberall, we have "
+print 11
+puts " great students."
