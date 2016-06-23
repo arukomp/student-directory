b2ece187fe882be15364032035194ee59633c3e2
Arunas Skirius <arukomp@gmail.com>
2016-06-23 11:39:50 +0100

    Added student loading from a file. Exercise 12

--- directory.rb
+++ directory.rb
@@ -10,6 +10,17 @@ def save_students
     file.puts csv_line
   end
   file.close
+  puts "Student list successfully saved!"
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, age, country = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, age: age, country: country}
+  end
+  file.close
+  puts "Student list successfully loaded!"
 end
 
 def input_students
@@ -99,6 +110,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -110,6 +122,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
